5c0fb23be32e7ab853a38f685034d3b509250ea7
MaximeG9 <gurbala.maxime@gmail.com>
2023-07-12 13:46:35 +0200

    quizz right traitement update

--- process/right_traitement.php
+++ process/right_traitement.php
@@ -1,6 +1,29 @@
+<?php var_dump($_POST); ?>
+
+
+<?php session_start(); ?>
+<?php require_once('../utils/connexion.php'); ?>
+
 <?php
 
-var_dump($_POST);
+if ( !empty($_POST['correct']) || !empty($_POST['wrong'])) {
 
+    if(!empty($_POST['idQuestion'])){
+        $query = 'SELECT * FROM questions WHERE id_questions = :idQuestion';
+        $request = $db->prepare($query);
+        $request->execute([
+            ':idQuestion' => $_POST['idQuestion'],
+        ]);
+        $question = $request->fetch();
+    }
+
+    if (isset($_POST['correct'])) {
+        echo "Bonne réponse";
+
+    } else {
+        echo "Mauvaise réponse";
+    }
+}
 
 ?>
+

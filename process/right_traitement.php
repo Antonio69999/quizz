3c7602d2e202bec491845600329147c71d4d9429
MaximeG9 <gurbala.maxime@gmail.com>
2023-07-12 16:43:06 +0200

    wrong right

--- process/right_traitement.php
+++ process/right_traitement.php
@@ -1,7 +1,9 @@
 <?php var_dump($_POST); ?>
+var
 
-
-<?php session_start(); ?>
+<?php session_start(); 
+var_dump($_SESSION);
+?>
 <?php require_once('../utils/connexion.php'); ?>
 
 <?php
@@ -17,11 +19,34 @@ if ( !empty($_POST['correct']) || !empty($_POST['wrong'])) {
         $question = $request->fetch();
     }
 
+    
     if (isset($_POST['correct'])) {
-        echo "Bonne réponse";
+        $id_users = $_SESSION['user']['id_user'];
+
+        $sql = "INSERT INTO `user_answers` (`user_answer`, `id_questions`, `id_user`) VALUES (:correct, :idQuestion, :id_user)";
+        $query = $db->prepare($sql);
+        $query->execute([
+            ':correct' => $_POST['correct'],
+            ':idQuestion' => $_POST['idQuestion'],
+            ':id_user' => $id_users 
+        ]);
+
+        $insertedId = $db->lastInsertId();
+
 
     } else {
         echo "Mauvaise réponse";
+        $id_users = $_SESSION['user']['id_user'];
+
+        $sql = "INSERT INTO `user_answers` (`user_answer`, `id_questions`, `id_user`) VALUES (:wrong, :idQuestion, :id_user)";
+        $query = $db->prepare($sql);
+        $query->execute([
+            ':wrong' => $_POST['wrong'],
+            ':idQuestion' => $_POST['idQuestion'],
+            ':id_user' => $id_users 
+        ]);
+
+        $insertedId = $db->lastInsertId();
     }
 }
 
